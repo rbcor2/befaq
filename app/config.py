14f9ca066bbd8eceff489969e6f9d4c21cd23f12
rbcor2 <rbcor2@users.noreply.github.com>
2017-06-27 17:03:49 +0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 SECRET_KEY = '873yiuwehrily-needs-to-be-changedopsskdjfjfk92837489634'
-SQLALCHEMY_DATABASE_URI = "sqlite:///befaq.db"
+SQLALCHEMY_DATABASE_URI = "postgresql://ulykdhukdpfoezegz4jw:EmXD5qadsZ8GWHiZKrWT@bqourgrcpebvy5b-postgresql.services.clever-cloud.com:5432/bqourgrcpebvy5b"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG = True
