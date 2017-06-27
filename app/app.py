c11708ca9622a6520027450916a9750b385c1579
rbcor2 <rbcor2@users.noreply.github.com>
2017-06-27 17:00:14 +0600

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify
+
+import config
+from models import session, Result
+
+app = Flask(__name__)
+app.config.from_object(config)
+
+@app.route('/')
+def index():
+    return "<center>alhamdulillah... its working!</center>"
+    
+@app.route('/<int:r>')
+def roll(r):
+    s = session.query(Result).filter_by(roll=r).first()
+    if s:
+        return jsonify({"messages": [{"text": "{}".format(s.res)}]})
+    else:
+        return jsonify({"messages": [{"text": "result not found"}]})
+
+@app.route('/41033/<int:start>/<int:stop>')
+def dataload(start, stop):
+    import dataloader
+    dataloader.executor.submit(dataloader.loader, start, stop)
+    return """<html><center>data loading started in background...</br>
+            with start value = {}</br>
+            and stop value = {} </br>
+            </br>
+            DO NOT HIT THIS URL AGEIN</html></center>""".format(start, stop)
+            
+            
+@app.route('/models')
+def models():
+    import models
+    return 'models created'
+    
+if __name__ == '__main__':
+    app.run()
+    
---
+++ app/dataloader.py
@@ -0,0 +1,47 @@
+import requests
+import config
+from concurrent.futures import ThreadPoolExecutor
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from helper import beautify
+from models import Result
+
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+executor = ThreadPoolExecutor(1)
+
+result_url = 'http://saharait.com/mark-sheet.php'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+payload = {}
+payload['years'] = 2017
+payload['ClassName'] = 2
+
+def loader(start, stop):
+    error_count = 0
+    for roll in range(start, stop+1):
+        if error_count > 5: break
+        try:
+            payload['Roll'] = roll
+            r = requests.post(result_url,payload,headers=headers)
+            r.encoding = 'utf-8'
+            result = beautify(r)
+            
+            data_row = Result(roll, result)
+            session.add(data_row)
+            session.commit()
+            print('roll {} added to database'.format(roll))
+        except:
+            error_count +=1
+            print('one error occured')
+            continue
+    if error_count > 4:
+        print( "data loading abort due to 5 error!!")
+    else:
+        print("Data loaded succesfully")
